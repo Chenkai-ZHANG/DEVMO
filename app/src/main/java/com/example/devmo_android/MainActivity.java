d14d96ebc92128dce231e1da3669e34649785526
ChenkaiZhang <zhang99662@gmail.com>
2021-03-22 10:12:21 +0100

    click between activity

--- app/src/main/java/com/example/devmo_android/MainActivity.java
+++ app/src/main/java/com/example/devmo_android/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.devmo_android;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -17,10 +19,16 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         final TextView mytextview = (TextView) findViewById(R.id.name1);
         mytextview.setOnClickListener(new View.OnClickListener() {
+           @Override
+            public void onClick(View v) {
+               MainActivity.this.startActivity(new Intent(MainActivity.this, SecondActivity.class));
+           }
+        });
+        final TextView mytextview1 = (TextView) findViewById(R.id.name2);
+        mytextview1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent myIntent = new Intent(Login.this,SecondActivity.class);
-                startActivity(myIntent);
+                MainActivity.this.startActivity(new Intent(MainActivity.this, ThirdActivity.class));
             }
         });
     }
